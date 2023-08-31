bb6082b44eea7dacc5509f085242559ab012dd00
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-08-31 15:22:36 +0200

    working on ui components

--- rq-client/src/app/components/Cars/CarComponent.tsx
+++ rq-client/src/app/components/Cars/CarComponent.tsx
@@ -6,10 +6,16 @@ const CarComponent = (
     {id,manufacturer,model,price,img,description,}:
     {id: number;manufacturer: string;model: string;price: number;img: string;description: string;}) => {
   return (
-   <article className='sm:col-span-6 lg:col-span-3'>
-        <h1>{model}</h1>
-        <h2>{manufacturer}</h2>
-        <Image layout='fill' src={img} alt={`An image of a ${manufacturer} ${model}`} />
+   <article className='sm:col-span-6 lg:col-span-3 h-96'>
+        <div className='w-auto h-64'>
+            <img className='object-center h-[inherit] object-cover' src={img} alt={`An image of a ${manufacturer} ${model}`} />
+            {/* <Image width={720} height={1280} className=' object-fill object-center ' src={img} alt={`An image of a ${manufacturer} ${model}`} /> */}
+        </div>
+        <section className='w-full h-32 bg-green-200'>
+            <h1 className='font-semibold text-zinc-800 text-2xl'>{model}</h1>
+            <h2 className='font-semibold text-zinc-700 text-xl'>{manufacturer}</h2>
+            <h2 className='font-semibold text-zinc-600 '>{`$${price.toLocaleString()}`}</h2>
+        </section>
    </article>
   )
 }
--- rq-client/src/app/page.tsx
+++ rq-client/src/app/page.tsx
@@ -2,7 +2,7 @@ import CarGrid from "./components/Cars/CarGrid";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <CarGrid/>
     </main>
   );
