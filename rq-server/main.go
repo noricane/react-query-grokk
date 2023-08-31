e902014a96a011fe1800185d9484b01cabab1418
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-08-31 13:44:54 +0200

    x

---
+++ rq-server/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"rq-server/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+func main() {
+
+	app := fiber.New()
+	var environment string
+	if env := os.Getenv("ENVIRONMENT"); env != "DEVELOPMENT" {
+		environment = "Production"
+		fmt.Print("Initializing cors\n")
+		app.Use(cors.New(cors.Config{
+			AllowOrigins: "https://app.unstxpbl.com, https://*.unstxpbl.com",
+			AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		}))
+	} else {
+		environment = "Development"
+
+		fmt.Println("Allow cors") 
+		app.Use(cors.New(cors.Config{
+			AllowOrigins: "*",
+			// AllowHeaders:  "Origin, Content-Type, Accept",
+		}))
+
+	}
+
+	setupRoutes(app)
+
+	fmt.Printf("Launching server in %s mode \n",environment)
+	if PORT := os.Getenv("PORT"); PORT != "" {
+		app.Listen(fmt.Sprintf(":%s", PORT))
+	} else {
+		listen(8000, app)
+	}
+}
+
+func listen(port int, app *fiber.App) {
+	fmt.Printf("Connecting to port %d\n", port)
+	err := app.Listen(fmt.Sprintf(":%d", port))
+	fmt.Printf("Failed to connect %v\n", err)
+	if err != nil {
+		listen(port+1, app)
+	}
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api", func(c *fiber.Ctx)error{
+		return c.Status(200).JSON("halo world")
+	})
+	app.Get("/api/something",routes.GetSomething)
+}
