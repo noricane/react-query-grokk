30ea83cccd6ec4e0e7a09c4d99f51071685fdb88
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-08-31 23:10:49 +0200

    get cars works

--- rq-server/routes/something.go
+++ rq-server/routes/something.go
@@ -1,19 +1,14 @@
 package routes
 
 import (
-	"fmt"
 	"os"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetCars(c *fiber.Ctx) error {
 	res,err := os.ReadFile("data/cars.json")
 	if err != nil {
-		fmt.Printf("Error: %+v\n",err)
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
-	//Use generator to read line by line?
-
 	return c.Status(200).Send(res)
 }
