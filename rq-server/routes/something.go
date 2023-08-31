7b4efdf30ee30e135cc94a4c37c3073fdf9b308d
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-08-31 23:08:50 +0200

    lol

--- rq-server/routes/something.go
+++ rq-server/routes/something.go
@@ -1,17 +1,19 @@
 package routes
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetCars(c *fiber.Ctx) error {
-	res,err := os.ReadFile("../data/cars.json")
+	res,err := os.ReadFile("data/cars.json")
 	if err != nil {
+		fmt.Printf("Error: %+v\n",err)
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 	//Use generator to read line by line?
-	
-	return c.SendStatus(fiber.StatusNotImplemented)
+
+	return c.Status(200).Send(res)
 }
