026e56798ace60db8a7f1da776c09a0b79352b20
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-09-02 13:06:14 +0200

    doing everything but learning query huh🤣

--- rq-client/src/app/components/User/LastSelectedComponent.tsx
+++ rq-client/src/app/components/User/LastSelectedComponent.tsx
@@ -6,27 +6,30 @@ import Spinner from '../Misc/Spinner';
 const LastSelectedComponent = ({data}:{data:DefinedUseQueryResult<number, unknown>}) => {
     const {data:lastSelected, isError,error,isFetching, failureCount } = data
     if (isError) return (
-        <div className='flex-1 w-screen flex flex-col justify-center items-center'>
-          <FaRegTimesCircle color="red" size={100}/>
+        <div className='h-48 col-span-full flex flex-col items-center justify-center w-full text-center bg-white rounded-xl'>
+          <FaRegTimesCircle color="red" size={50}/>
           {/* Here one should check if it's good practice to display error message to user,
               but in this case this is no problem */}
+          <div>Error getting the last selected car from the server</div>
           <div>{(error as unknown&{message:string}).message}</div>
         </div>);
     
       if (failureCount > 0) return (
-        <>
+        <div className='h-48 col-span-full flex flex-col items-center justify-center w-full text-center bg-white rounded-xl'>
+
           <Spinner error={true}/>
           <div>Failed, trying again</div>
-        </>
+        </div>
     );
     
       if (isFetching == true) return (
-        <>
+        <div className='h-48 col-span-full flex flex-col items-center justify-center w-full text-center bg-white rounded-xl'>
+
           <Spinner error={!true}/>
           Loading last selected
-        </>);
+        </div>);
   return (
-    <div className='h-12 col-span-full flex items-center justify-center w-full text-center bg-white rounded-xl'>{lastSelected}</div>
+    <div className='h-48 col-span-full flex items-center justify-center w-full text-center bg-white rounded-xl'>{lastSelected}</div>
   )
 }
 
