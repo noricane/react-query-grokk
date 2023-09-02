8b565a3c775c1a2e9fae6d6b5f2fff82c040c614
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-09-02 15:43:20 +0200

    documented

--- rq-client/src/app/components/Cars/CarComponent.tsx
+++ rq-client/src/app/components/Cars/CarComponent.tsx
@@ -7,8 +7,8 @@ const CarComponent = (
     {id,manufacturer,model,price,img,description,onClick,wiki}:
     {id: number;manufacturer: string;model: string;price: number;img: string;description: string;onClick:React.MouseEventHandler<HTMLElement>,wiki:string}) => {
   return (
-   <article className=' hover:scale-105  transition-transform relative utsm:col-span-full sm:col-span-6 lg:col-span-3 h-96'>
-    <div  onClick={onClick} className=' active:scale-100 transition-transform  bg-zinc-50 select-none !cursor-pointer rounded-lg overflow-hidden'>
+   <article className=' hover:scale-105 hover:z-10 duration-300 transition-transform relative utsm:col-span-full sm:col-span-6 lg:col-span-3 h-96'>
+    <div  onClick={onClick} className=' active:brightness-95 hover:z-10 active:scale-[1.06] transition-all  bg-zinc-50 select-none !cursor-pointer rounded-lg overflow-hidden'>
         <div className='w-full h-64'>
            { <img draggable={false} className=' object-center w-[inherit] h-[inherit] object-cover' src={img} alt={`An image of a ${manufacturer} ${model}`} />}
             {/* <Image width={720} height={1280} className=' object-center w-[inherit] h-[inherit] object-cover' src={img} alt={`An image of a ${manufacturer} ${model}`} /> */}
@@ -23,9 +23,10 @@ const CarComponent = (
         
         </div>
     </div>
-    <button className='w-36 active:bg-red-200 h-12 absolute bottom-4 right-4 bg-black rounded-lg text-white font-semibold' onClick={(e)=>{e.stopPropagation();console.log("Send to server")}}>
+    {/* Button doesn't scale when clicking the card, minor ui issue. */}
+    <button className='w-36 active:bg-red-200 h-12 transition-transform absolute bottom-4 right-4 bg-black rounded-lg text-white font-semibold' onClick={(e)=>{e.stopPropagation();console.log("Send to server")}}>
             Select Car
-        </button>
+    </button>
    </article>
   )
 }
