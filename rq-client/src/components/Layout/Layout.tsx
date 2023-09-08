2fb8c391739a9dbc5cb0645b31613fadf03aa098
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-09-08 11:07:37 +0200

    Your commit message

--- rq-client/src/components/Layout/Layout.tsx
+++ rq-client/src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react'
 import StoreContext from '../context/StoreContext';
-import 
+
 const Layout = ({children}:{children:React.ReactNode}) => {
   const [queryClient,] = React.useState(()=>new QueryClient()) 
   const store = configureStore()
