f01111e9c6085289f81c109b40403a2549453f1f
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-09-09 11:07:02 +0200

    luh

--- rq-client/src/components/Layout/Layout.tsx
+++ rq-client/src/components/Layout/Layout.tsx
@@ -1,14 +1,17 @@
 "use client"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react'
-
+import StoreContext from '../context/StoreContext';
+import 
 const Layout = ({children}:{children:React.ReactNode}) => {
   const [queryClient,] = React.useState(()=>new QueryClient()) 
-
+  const store = configureStore()
   return (
-    <QueryClientProvider client={queryClient}>
+    <StoreContext.Provider value={StoreContext}>
+      <QueryClientProvider client={queryClient}>
         {children}
-    </QueryClientProvider> 
+    </QueryClientProvider>
+    </StoreContext.Provider> 
   )
 }
 
---
+++ rq-client/src/components/context/StoreContext.tsx
@@ -0,0 +1,5 @@
+import React,{createContext} from 'react'
+/* @ts-ignore */
+const StoreContext = createContext()
+
+export default StoreContext
